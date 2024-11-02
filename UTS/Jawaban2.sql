6002f7da2ea5c0150520427542ea6e14b7046655
Maulana Rangga Arrosyid & Cira Ramadhanti <ramadhanticira@gmail.com>
2024-11-02 22:38:21 +0700

    Add files via upload

---
+++ UTS/Jawaban2.sql
@@ -0,0 +1,26 @@
+INSERT	INTO	fakultas_id
+VALUES	(
+	(1)
+);
+
+INSERT	INTO	mahasiswa
+VALUES 
+	(0801234, "Maulana", "Albert", "Ilmu Komputer", 1, "Teknik Informatika"),
+	(0801045, "Toto", "Jhonson", "Ilmu Komputer", 1, "Sistem Informasi"),
+	(0801022, "Joe", "Cenat", "Ilmu Komputer", 1, "Sistem Informasi"),
+	(0801552, "Abdul", "Andreas", "Ilmu Komputer", 1, "Teknik Informatika");
+
+INSERT	INTO	dosen
+VALUES
+	(085, "Wahyu", "Clark"),
+	(083, "Diah", "Santoso"),
+	(087, "Rangga", "Galih"),
+	(081, "Bruce", "Nath"); 
+	
+INSERT	INTO	mata_kuliah
+VALUES
+	(1083, "Algorithm", 081, 4, 1),
+	(1087, "Data Structure", 083, 3, 1),
+	(1089, "Database", 081, 3, 1),
+	(1084, "Machine Learning", 087, 2, 1),
+	(1081, "Software Engineering", 085, 5, 1);
